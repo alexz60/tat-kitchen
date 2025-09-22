a58aab73b4afa467d544e002a6ca4891c435f05b
alexz60 <alexqm@mail.ru>
2025-09-22 11:15:49 +0300

    fix token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { getToken, GetTokenParams } from 'next-auth/jwt';
 
 export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const token = await getToken({
+
+    let params: GetTokenParams = {
         req: request,
         secret: process.env.AUTH_SECRET,
-    });
+    };
 
-    if (config.matcher.some((route) => pathname.startsWith(route.replace(':path*', '')))) {
+    if (process.env.NODE_ENV === 'production') {
+        params = {
+            ...params,
+            cookieName: '__Secure-authjs.session-token',
+        };
+    }
+
+    const token = await getToken(params);
+
+    if (
+        config.matcher.some((route) =>
+            pathname.startsWith(route.replace(':path*', ''))
+        )
+    ) {
         if (!token) {
             const url = new URL('/error', request.url);
             url.searchParams.set('message', 'Недостаточно прав');
