9b4bd166c47f49aada2268a26c274e02b4ac0501
alexz60 <alexqm@mail.ru>
2025-09-22 09:11:24 +0300

    update type

--- src/types/ingredient.ts
+++ src/types/ingredient.ts
@@ -5,6 +5,6 @@ export interface IIngredient {
     unit: string;
     pricePerUnit: number | null;
     description: string | null;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
